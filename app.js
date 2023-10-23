31298cac7a9a729c788cdab1e0302c7f184272b4
ADAWAS <pasitsupholkhan@gmail.com>
2023-10-23 18:31:53 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,10 @@
 console.log("Hello");
 
+<<<<<<< HEAD
 data = [2, 3, 4];
 json = { dd: 2 };
+=======
+data = [1,2,3]
+
+data.map((val)=>console.log(val))
+>>>>>>> parent of 41fb7f0 (Merge pull request #2 from PRIMEADAWAS/developer)
